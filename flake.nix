415e5d1aff0864769359f118ce8c218daca60ad8
niccogc <92951857+niccogc@users.noreply.github.com>
2026-02-12 23:25:14 +0000

    chore: update plugins and hashes

--- flake.nix
+++ flake.nix
@@ -84,9 +84,9 @@
     packages.${system} = {
       oh-my-opencode = mkOpenCodePlugin {
         pname = "oh-my-opencode";
-        version = "3.5.2";
+        version = "3.5.3";
         src = oh-my-opencode-src;
-        outputHash = "sha256-TazHp4zvclIh7aPQqz9GQ2rSoAgN0kYYJ4wOQ5YPCxI=";
+        outputHash = "sha256-agFAByv56DGQjqPbzCixYAIFvnCATMZXTx3ujHFLev4=";
       };
       antigravity = mkOpenCodePlugin {
         pname = "opencode-antigravity-auth";
